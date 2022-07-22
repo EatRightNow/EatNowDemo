df08c77c34a0bfa5168842870cd30876e6de44fb
XuYidan <xuyidan1701@163.com>
2022-07-22 17:07:24 +0800

    因为我把Demo1弄坏了 所以重新建了个项目

---
+++ EatNowDemo2/src/main/java/com/example/eatnowdemo2/EatNowDemo2Application.java
@@ -0,0 +1,13 @@
+package com.example.eatnowdemo2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EatNowDemo2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EatNowDemo2Application.class, args);
+    }
+
+}
