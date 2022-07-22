4fe4d25f9b0bbe8330d27528f97d5f7b31867884
XuYidan <xuyidan1701@163.com>
2022-07-22 16:17:26 +0800

    创建了徐艺丹的测试页面/test/test1.html

---
+++ EatNowTestDemo/src/main/java/com/example/eatnowtestdemo/EatNowTestDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.eatnowtestdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EatNowTestDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EatNowTestDemoApplication.class, args);
+    }
+
+}
---
+++ EatNowTestDemo/src/test/java/com/example/eatnowtestdemo/EatNowTestDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.eatnowtestdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EatNowTestDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
